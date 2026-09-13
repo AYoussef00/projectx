3b9727d8bde31404374b90bcb9cbc226899785c7
AYoussef00 <abdelrahman.yousef@hadaf-hq.com>
2026-09-13 10:29:22 +0300

    edit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,7 +6,6 @@ use App\Http\Middleware\RedirectToInformSubdomain;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -22,10 +21,10 @@ return Application::configure(basePath: dirname(__DIR__))
             RedirectToInformSubdomain::class,
         ]);
 
+        // Avoid huge Link preload headers that break nginx (502: upstream sent too big header)
         $middleware->web(append: [
             HandleAppearance::class,
             HandleInertiaRequests::class,
-            AddLinkHeadersForPreloadedAssets::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
